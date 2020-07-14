ecd4e399c4d3a1ef3a1d2192121085b53aeae32c
MohamedZiidan <mohamedzidan000098@gmail.com>
2020-07-14 17:54:33 +0200

    added some code

--- app/src/main/java/com/example/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/android/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "Hello from android", Toast.LENGTH_LONG).show()
 
-
+        val gson = GsonBuilder().create()
 
     }
 }
